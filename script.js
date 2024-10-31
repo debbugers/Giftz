db918dc5df4a77b9996227cad4052b8e19975c82
Harsh Kumar <142787311+Harsh0166@users.noreply.github.com>
2024-10-31 17:46:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+  var right = document.getElementById("right_move");
+  var left = document.getElementById("left_move");
+  var img_move = document.getElementById("img_move");
+  // var img_move = document.getElementById("image");
+
+  left.onclick = function(){
+    img_move.scrollBy(-200,0);
+  }
+  right.onclick = function(){
+    img_move.scrollBy(200,0);
+  }
+// function demo(x){
+//   document.getElementById("img_move").scrollBy(x,0);
+// }
